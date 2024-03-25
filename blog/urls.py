dd4ff7000978a0c30e1739585252eab90c122dc7
Mr1-D3CRYPT <ashishthomas06@gmail.com>
2024-03-25 11:44:33 +0530

    Something

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.post_list),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('drafts/', views.draft, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.utils import timezone
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Post
 
 def post_list(request):
@@ -9,3 +9,16 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'post_detail.html', {'post': post})
+
+def draft(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
